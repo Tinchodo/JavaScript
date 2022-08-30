4a3117269e10cf1407597cf673f00f0b9de1a863
Tinchodo <tinchodoval@gmail.com>
2022-08-29 21:23:14 -0300

    javaScript

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-jQuery("document").ready(function($) {
-    var menuBtn = $(".menu-icon"),
-    menu = $("navigation ul");
+let continente = prompt( "Hola, eres de europa o america?")
+let alcohol = prompt("Deseas comprar ron o vodka?")
+let total
+let ron = 1200
+let vodka = 1500
 
-    menuBtn.click(function() {
-        menu.addClass("show");
+if(continente === "europa"){
+    iva = 21
+    } if(alcohol === "ron"){
+        total = ron + ((iva/100)*ron)
+    } else if(alcohol === "vodka"){
+        total = vodka + ((iva/100)*vodka)
+    }else{
+        alert("No tenemos ese producto")
+    }
 
-    });
-    
-});
+if(continente === "america"){
+    iva = 0
+    } if(alcohol === "ron"){
+        total = ron
+    } else if(alcohol === "vodka"){
+        total = vodka
+    } 
 
+    else{
+    alert("No hacemos envios a esa zona")
+}
 
-
-
-// let tereceraVariable = "primera variable con let"
-// const primeraConstante = 4
-// const segundaConstante = 7
-// let resultado = primeraConstante+segundaConstante
-// console.log(resultado)
+alert("El envio a "+continente+" tiene un costo de iva del "+iva+"%, y su total es "+total+"$ ,muchas gracias por su compra!")
